270001e55324c127fbfa7d0efa4cd0135ec6294f
jinusong <sugwer7464@gmail.com>
2019-03-18 19:16:16 +0900

    [DELETE] Project

--- Code Pattern/MVP/MemoApplication/app/src/main/java/com/jinwoo/memoapplication/RecyclerViewClickListener.kt
+++
@@ -1,8 +0,0 @@
-package com.jinwoo.memoapplication
-
-import com.jinwoo.memoapplication.Model.MemoModel
-
-interface RecyclerViewClickListener{
-    fun onItemClicked(position: Int, memo: MemoModel, key: String)
-    fun onItemLongClicked(position: Int, memo: MemoModel, key: String)
-}
